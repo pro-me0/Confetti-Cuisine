d75c1b85d4e43c5081444f2858fe372a89a5b014
pro-me0 <mazisomtochukwu@gmail.com>
2022-08-04 18:59:10 +0100

    line idicator

--- main.js
+++ main.js
@@ -71,6 +71,7 @@ app.use("/", router);
 
 const server = app.listen(app.get("port"), () => {
   console.clear()
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>')
     console.log(`Server running at http://localhost:${app.get("port")}`);
   }),
   io = require("socket.io")(server),
